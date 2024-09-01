f76580c6209f5c07208750f4de3ea993b58f8990
Moamen Hraden <moamen.hraden@primion.eu>
2024-09-01 18:38:16 +0200

    first commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::collections::HashMap;
+
+struct Message<T> {
+    content_length: u64,
+    message: T
+}
+
+
+struct SimpleMessage<String>{
+}
+
 fn main() {
-    println!("Hello, world!");
+
 }
